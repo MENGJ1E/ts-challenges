c4a6f3572f1a2a02f8fc56199b005a4ed8b7fa75
章梦洁 <11111>
2023-06-02 09:23:38 +0800

    feat: 配置webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,20 +1,24 @@
 const path = require('path')
 const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
-    path: './src/main.js',
+    path: './src/main.ts',
   },
   module: {
     rules: [
+      // 使用vue-loader处理vue文件
       {
         test: /\.vue$/,
         use: 'vue-loader',
       },
+      // syleloade解析css文件
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      // 使用loader解析图片文件
       {
         test: /\.(png|jpe?g|gif)$/,
         type: 'asset/resource',
@@ -22,11 +26,15 @@ module.exports = {
           filename: 'assets/img/[hash][ext]'
         }
       },
+      // webpack集成ts开发，webpcak不识别ts文件，需要借助ts-loader解析
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+        test:/\.tsx?$/,
+        loader:"ts-loader",
+        options: {
+            configFile: path.resolve(process.cwd(), 'tsconfig.json'),
+            appendTsSuffixTo: [/\.vue$/]
+         },
+     }
     ],
   },
   resolve: {
@@ -37,6 +45,9 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
   },
   plugins: [
-      new VueLoaderPlugin()
-  ]
+      new VueLoaderPlugin(),
+      new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, './index.html')
+      })
+  ] 
 }
