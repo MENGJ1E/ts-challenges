c689b3b6d9990dc58b4c2fcaeb527a4f507258c6
章梦洁 <11111>
2023-07-26 17:58:24 +0800

    feat: 删除不需要文件，完成Pick方法以及元组类型变成对象

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,32 @@
 <script setup lang="ts">
-// import TheWelcome from '../components/TheWelcome.vue'
+// // 实现Pcik
+// interface Todo {
+//   title: string
+//   description: string
+//   completed: boolean
+// }
+
+// type MyPick<T, P extends keyof T = keyof T> = {
+//   readonly [K in P]: T[K]
+// }
+
+// type TodoPreview = MyPick<Todo, 'title' | 'completed'>
+
+// const todo: TodoPreview = {
+//   title: 'Clean room',
+//   completed: false,
+// }
+
+// // 接收数组 对应的数组类型
+// const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+
+// type TupleToObject<T extends readonly any[]> = {
+//   readonly [K in T[number]]: K
+// }
+
+// type result = TupleToObject<typeof tuple> // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+
+
 </script>
 
 <template>
